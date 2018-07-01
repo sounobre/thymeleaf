163aba6eb79f2d22fdbe092abbda376b4cdeb68d
Diego Nobre <sounobre@hotmail.com>
2018-07-01 20:47:32 -0300

    testte

--- src/main/java/com/example/teste/controller/CityController.java
+++ src/main/java/com/example/teste/controller/CityController.java
@@ -6,10 +6,16 @@
 package com.example.teste.controller;
 
 
+import com.example.teste.model.City;
+import com.example.teste.repository.CityRepository;
+import javax.persistence.PostLoad;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -20,13 +26,28 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("cities")
 public class CityController {
     
+    @Autowired
+    private CityRepository cityRepository;
+    
+    public CityController(CityRepository cityRepository){
+        this.cityRepository = cityRepository;
+    }
+    
     @GetMapping
     public String index(Model model){
+        model.addAttribute("cities", this.cityRepository.findAll());
         return "cities/index";
+    }
+    
+    @PostMapping
+    public String create(@ModelAttribute("city") City city, Model model){
+      this.cityRepository.save(city);
+        return "redirect:/cities";
     }
     
     @GetMapping("/new")
     public String newCity(Model model) {
+        model.addAttribute("city", new City());
         return "cities/new";
     }
     
